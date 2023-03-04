269e8cb4883a8ad1f34da14016f194ac26561b4d
Jade Remi <jadereminiscent@yandex.ru>
2023-03-04 14:49:11 +0100

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import InitSqlJs from "sql.js";
 
 export function App () {
 
-    async function retr () {
+    async function retr() {
         const SQL = await InitSqlJs({
             locateFile: file => `https://sql.js.org/dist/${file}`
         });
         const db = new SQL.Database;
+        return db;
     }
 
+    retr().then(db => console.log('DB up!: ', db))
+
+    
 
     return (
         <div />
